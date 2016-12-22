e3a6abd18200b56d73e1728cadeb627197099186
Alfred D'souza <Alfred D'souza>
2016-12-22 15:28:45 +0000

    update API url

--- angular_fetch_data/modules/apiCall/acService.js
+++ angular_fetch_data/modules/apiCall/acService.js
@@ -1,6 +1,6 @@
 fetchDataApp.service( 'acService', function( $http, $q ) {
   var deferred = $q.defer();
-  $http.get( 'http://swapi.co/api/starships/9000/' ).then( function( response ) {
+  $http.get( 'http://swapi.co/api/starships/9/' ).then( function( response ) {
 	  deferred.resolve( response );
   }, function( response ) {
     console.log(response);
